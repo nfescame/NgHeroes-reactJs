4733e952d7e715ffdf958b0f91d2e0d85f46efd7
Guilherme Rosa <guilhermebr84@gmail.com>
2021-09-14 12:19:16 -0300

    alterações junto com o pedro

--- src/components/ListHeroes.js
+++ src/components/ListHeroes.js
@@ -9,6 +9,9 @@ import NewPlayer from "./NewPlayer";
 class ListHeroes extends React.Component {
   state = {
     allHeroes: [],
+    playerName: "",
+    squadName: "",
+    allFavFive: [],
   };
 
   componentDidMount = async () => {
@@ -41,42 +44,104 @@ class ListHeroes extends React.Component {
     }
   };
 
+  handleChangeName = (event) => {
+    this.setState({ playerName: event.target.value });
+  };
+
+  handleChangeSquad = (event) => {
+    this.setState({ squadName: event.target.value });
+  };
+
+  handleChangeHero = (event) => {
+    if (this.state.allFavFive.length < 5) {
+      let allFavFiveClone = [...this.state.allFavFive];
+      console.log(event.target.name);
+      allFavFiveClone.push(event.target.name);
+      this.setState({ allFavFive: [...allFavFiveClone] });
+    }
+  };
+
+  handleSubmitAll = (event) => {
+    event.preventDefault();
+    console.log(this.state);
+    if (this.state.allFavFive.length === 5) {
+      const stateClone = { ...this.state };
+      delete stateClone.allHeroes;
+      axios
+        .post("https://ironrest.herokuapp.com/NGHeroes", stateClone)
+        .then((response) => console.log(response))
+        .catch((err) => console.error(err));
+    }
+  };
+
   render() {
     return (
-    <div>
-
       <div>
-        <NewPlayer />
-      </div>
+        <div>
+          <NewPlayer
+            handleChangeName={this.handleChangeName}
+            state={this.state}
+            handleChangeSquad={this.handleChangeSquad}
+            handleSubmitAll={this.handleSubmitAll}
+          />
+        </div>
 
-      <div className='row mx-2'>
-        <SearchBar filterHeroesName={this.filterHeroesName} />
-        {this.state.allHeroes.map((hero) => {
-          return (
-            <Link className='event' to={`/details/${hero.id}`} key={hero.id}>
-              <img
-                src={hero.images.md}
-                alt='description'
-                style={{ width: "100%" }}
-              />
+        <div className="row mx-2">
+          <SearchBar filterHeroesName={this.filterHeroesName} />
+          {this.state.allHeroes.map((hero) => {
+            return (
+              {
+                /* <Link className="event" to={`/details/${hero.id}`} key={hero.id}> */
+              },
+              (
+                <div className="event">
+                  <img
+                    src={hero.images.md}
+                    alt="description"
+                    style={{ width: "100%" }}
+                  />
 
-              <div className='content'>
-                <h3>{hero.name}</h3>
+                  <div className="content">
+                    <h3>{hero.name}</h3>
+                    <div
+                      className="btn-group"
+                      role="group"
+                      aria-label="Basic checkbox toggle button group"
+                    >
+                      <input
+                        type="checkbox"
+                        className="btn-check"
+                        id="btncheck1"
+                        autocomplete="off"
+                        name={hero.id}
+                        onChange={this.handleChangeHero}
+                      />
+                      <label
+                        className="btn btn-outline-primary"
+                        for="btncheck1"
+                      >
+                        Add
+                      </label>
+                    </div>
 
-                <div className='rollover'>
-                  <p>Full Name: {hero.biography.fullName}</p>
-                  <p>First Appearance: {hero.biography.firstAppearance}</p>
-                  <p>Publisher: {hero.biography.publisher}</p>
-                  <p>Place Of Birth: {hero.biography.placeOfBirth}</p>
-                  <p>Group Affiliation: {hero.connections.groupAffiliation}</p>
-                  <h3>Click for more details.</h3>
+                    <div className="rollover">
+                      <p>Full Name: {hero.biography.fullName}</p>
+                      <p>First Appearance: {hero.biography.firstAppearance}</p>
+                      <p>Publisher: {hero.biography.publisher}</p>
+                      <p>Place Of Birth: {hero.biography.placeOfBirth}</p>
+                      <p>
+                        Group Affiliation: {hero.connections.groupAffiliation}
+                      </p>
+                      <h3>Click for more details.</h3>
+                    </div>
+                  </div>
+                  {/* </Link> */}
                 </div>
-              </div>
-            </Link>
-          );
-        })}
+              )
+            );
+          })}
+        </div>
       </div>
-    </div>
     );
   }
 }
