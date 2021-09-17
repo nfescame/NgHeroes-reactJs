1ff500331005c0bee38599c6ab33c61a097bfcee
nilton <nfescame@gmail.comq:qclear>
2021-09-17 10:32:23 -0300

    the end

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,9 @@ function Footer() {
     <div>
       <footer className='footer-distributed bg-dark'>
         <div className='footer-left'>
-          <p className='footer-company-name'>NG &copy; 2021</p>
+          <p className='footer-company-name'>
+            Nilton &amp; Guilherme &copy; 2021
+          </p>
         </div>
       </footer>
     </div>
